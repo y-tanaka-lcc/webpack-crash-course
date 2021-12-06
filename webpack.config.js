b7df469406287613af43968113b9e5506616db31
Yuki Tanaka <tanaka_yuki@lightcafe.co.jp>
2021-12-06 15:43:18 +0900

    url-loaderで画像を取り込み、file-loaderで画像をファイルとして取り込む

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico|)$/i,
+        loader: "url-loader",
+        options: {
+          limit: 1024,
+          name: "./images/[name].[ext]",
+        },
+      },
     ],
   },
   devServer: {
